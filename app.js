75d43ffa1a8ee2551c5a4853fc37bb0867d310bb
Mayowa-jerrie <108583250+Mayowa-jerrie@users.noreply.github.com>
2023-03-02 12:32:23 +0100

    My commit on J.S

---
+++ app.js
@@ -0,0 +1,14 @@
+(function(){
+
+    let screen = document.querySelector(".scren");
+    let buttons = document.querySelectorAll(".btn");
+    let clear = document.querySelector(".btn-clear");
+    let equal = document.querySelector(".btn-equal");
+
+    buttons.forEach(function(button){
+        button.addEventListener('click', function(e){
+            let value = 
+        })
+    })
+
+})();
